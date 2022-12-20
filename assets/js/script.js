8a2a6287da9110e67026f3160f18e62fe2a8a025
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-20 10:27:44 -0500

    fetch is working with api, styled site and discussed project requirements, thinking of using Bulma as a framework and a landing page with buttons that link to a search page, gallery page, navbar and footer with some content

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Selecting our HTML Element from DOM
+document.querySelector("#search").addEventListener("click", getPokemon);
+
+// Listening for event click, runs getPokemon function
+
+//Function capitalize first letter of search
+function capitalizeCase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lowerCase(string) {
+  return string.toLowerCase();
+}
+
+// Function fetches data from Pokemon API
+function getPokemon(e) {
+  const name = document.querySelector("#pokemonName").value;
+  const pokemonName = lowerCase(name);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+      <div>
+        <img src="${data.sprites.other["official-artwork"].front_default}" alt="Image of Pokemon">
+      </div>
+      <div class="pokemonInfos">
+        <h1>${capitalizeCase(data.name)} (#${data.id})</h1>
+        <p>Weight: ${data.weight}</p>
+        <p>Height: ${data.height}</p>
+        <p>Base Experience: ${data.base_experience}</p>
+
+      </div>
+      `;
+    })
+    .catch((err) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+      <h4>Pokemon not found.</h4>
+      `;
+      console.log("Error", err);
+    });
+  e.preventDefault();
+}
+
+
+
+
+
+
+
+
